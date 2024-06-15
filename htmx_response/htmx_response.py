b61377b3a9e0ae08d4f1acd7e0e42f45f1b6da1a
Gabe Levasseur <11285350+GabeL7r@users.noreply.github.com>
2024-06-14 22:12:10 -0400

    feat: use a response instead of api route

--- htmx_response/htmx_response.py
+++ htmx_response/htmx_response.py
@@ -1,41 +1,49 @@
 from fastapi import Request, Response
-from fastapi.routing import APIRoute
-from typing import Callable
+from fastapi.responses import JSONResponse
 from accept_types import get_best_match
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+from typing import Mapping
 
-import jinja2
-import json
 
-def get_htmx_route(
-        base_dir: str,
-        base_template: str,
-):
-    class HTMXRoute(APIRoute):
-        def get_route_handler(self) -> Callable:
-            original_route_handler = super().get_route_handler()
+class HTMXResponse(Response):
+    def __init__(
+        self,
+        request: Request,
+        content: BaseModel,
+        directory: str,
+        template: str,
+        status_code: int = 200,
+        headers: Mapping[str, str] | None = None,
+        media_type: str | None = None,
+    ) -> None:
+        self.directory = directory
+        self.template = template
+        self.content = content
+        self.request = request
+        super().__init__(content, status_code, headers, media_type, None)
 
-            async def custom_route_handler(request: Request) -> Response:
-                return_type = get_best_match(
-                    request.headers.get('accept', 'text/html'), ['text/html', 'application/json']
-                )
+    def render(self, content: BaseModel) -> bytes:
+        accept = self.request.headers.get('accept')
+        return_type = get_best_match(
+            accept, ['text/html', 'application/json']
+        )
 
-                response: Response = await original_route_handler(request)
-                if return_type == 'text/html' and 'htmx-template' in response.headers:
-                    env = jinja2.Environment(
-                        loader=jinja2.ChoiceLoader([jinja2.FileSystemLoader(base_dir)])
-                    )
-                    templates = Jinja2Templates(env=env)
-                    #templates = Jinja2Templates(directory=template_dirs)
-                    layout = templates.env.get_template(base_template)
-                    context = json.loads(response.body.decode('utf-8'))
-                    context['request'] = request
-                    context['layout'] = layout
-                    return templates.TemplateResponse(
-                        response.headers.get('htmx-template'),
-                        context=context
-                    )
-                return response
+        if return_type == 'text/html':
+            templates = Jinja2Templates(directory=self.directory)
 
-            return custom_route_handler
-    return HTMXRoute
+            # NOTE: you can add extra parameters to the request.state
+            # to be rendered in your jinja template
+            context = {'request': self.request, **self.request.state.context}
+            context['model'] = self.content
+
+            if isinstance(self.request.state.template_filters, dict):
+                for key, value in self.request.state.template_filters.items():
+                    templates.env.filters[key] = value
+            return templates.TemplateResponse(
+                self.template,
+                context,
+                status_code=self.status_code
+            ).body
+        elif return_type == 'application/json':
+            return JSONResponse(self.content)
